f367843760cbf57df0f1e8fe1ae03c8108d2c7a5
wirginiavoolf <stephzanie@gmail.com>
2017-02-04 15:54:34 -0500

    week 2 homework

---
+++ week 2/blink/blink.ino
@@ -0,0 +1,24 @@
+void setup(){
+//put your setup code here to run once
+  pinMode(13,OUTPUT);
+  //number indicates the pin
+}
+
+void loop(){
+//runs infinitely
+  digitalWrite(13,HIGH);
+  //high means turn on
+  delay(250);
+  //inside delay, add time in miliseconds
+  //delays STOP microcontroller, so eventually we will not use them
+  digitalWrite(13,LOW);
+  delay(1000);
+
+}
+
+//select Arduino/Genuino Uno
+//select Port (Port 3?)
+//click check mark to compile program
+//click right arrow to upload program
+//while uploading, TX & RX lights will go "crazy"
+
