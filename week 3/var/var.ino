68427605fe85b8ddc1e96d26497792cd12ad701f
wirginiavoolf <stephzanie@gmail.com>
2017-02-19 21:22:04 -0500

    great big upload
    2 or 3 weeks worth of gits

---
+++ week 3/var/var.ino
@@ -0,0 +1,24 @@
+int num = 500;
+long num2 = 1000000;
+float decimal = 1.2345;
+byte number = 254;
+unsigned int num3 = 64;
+boolean statment = true;
+char letter = 'A';
+//string words = "hello";
+String words2 = "goodbye";
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.print("hello world");
+  Serial.println(num);
+  Serial.println(letter);
+  Serial.print("num: ");
+  Serial.println(num);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
